d9c46996925f9bb5337e4614daeae834d075c39e
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-07 16:39:14 +0900

    added albumcell

--- AlbumsApp/AlbumsApp/ContentView.swift
+++ AlbumsApp/AlbumsApp/ContentView.swift
@@ -7,9 +7,18 @@
 
 import SwiftUI
 
+extension Album: Identifiable {
+    var id: String { albumTitle }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        List {
+            ForEach (AlbumStore.singleton.albums) { album in
+                AlbumCell(album: album)
+            }
+        }
+        Text("Hello, world! \(AlbumStore.singleton.albums.count)")
             .padding()
     }
 }
@@ -17,5 +26,22 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AlbumCell: View {
+    let album: Album
+    var body: some View {
+        HStack {
+            Image(systemName: "wifi")
+            VStack {
+                Text(album.albumTitle)
+                    .font(.subheadline)
+                    .foregroundColor(Color.blue)
+                    .multilineTextAlignment(.leading)
+                Text(album.artistName)
+                    .multilineTextAlignment(.leading)
+            }
+        }
     }
 }
