cb77e18af105286aee0bb81f2af41d12ff00d3e5
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-07 14:20:58 +0900

    국가/대륙 정보를 Nations.swift 로 옮김.

--- ListApp/ListApp/MainView.swift
+++ ListApp/ListApp/MainView.swift
@@ -7,50 +7,11 @@
 
 import SwiftUI
 
-struct Country: Identifiable {
-    var id: String { name }
-    let name: String
-}
-
-struct Continent: Identifiable {
-    let region: String
-    let countries: [Country]
-    var id: String { region }
-}
-
 struct MainView: View {
-    let continents = [
-        Continent(region: "Hello Continent", countries: [
-            Country(name:"Egypt"),
-            Country(name:"Guinea-Bissau"),
-            Country(name:"Guinée"),
-            Country(name:"Guinée Equatoriale"),
-            Country(name:"India"),
-            Country(name:"Israel"),
-            Country(name:"Jordan"),
-            Country(name:"Kenya"),
-            Country(name:"Kuwait"),
-            Country(name:"Madagascar"),
-            Country(name:"Mali"),
-            Country(name:"Maroc"),
-            Country(name:"Mauritius"),
-            Country(name:"Mozambique"),
-        ]),
-        Continent(region: "World Continent", countries: [
-            Country(name:"Niger"),
-            Country(name:"Nigeria"),
-            Country(name:"Oman"),
-            Country(name:"Qatar"),
-            Country(name:"Saudi Arabia"),
-            Country(name:"Sénégal"),
-            Country(name:"Uganda"),
-            Country(name:"South Africa"),
-            Country(name:"Tunisie"),
-        ]),
-    ]
+
     var body: some View {
         List {
-            ForEach (continents) { continent in
+            ForEach (Nations.continents) { continent in
                 Section(header: Text(continent.region)) {
                     ForEach (continent.countries) { country in
                         Text(country.name)
