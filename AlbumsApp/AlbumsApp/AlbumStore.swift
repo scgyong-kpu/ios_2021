70fc530923ea6ee052c9a0b77ad99297306389de
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-07 15:46:45 +0900

    AlbumStore / singleton 을 이용하여 load 시작하는 구조 만들기

---
+++ AlbumsApp/AlbumsApp/AlbumStore.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumStore.swift
+//  AlbumsApp
+//
+//  Created by Kiyong Kim on 2021/07/07.
+//
+
+import Foundation
+
+class AlbumStore {
+    static let singleton = AlbumStore()
+    func load() {
+        print("Loading...")
+    }
+}
