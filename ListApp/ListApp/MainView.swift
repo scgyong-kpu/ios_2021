e487adf6469738b1b3cfdb4532edb112a85eebbb
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-07 13:53:35 +0900

    나라이름 추가 (에러 있음)

--- ListApp/ListApp/MainView.swift
+++ ListApp/ListApp/MainView.swift
@@ -7,22 +7,36 @@
 
 import SwiftUI
 
-extension String: Identifiable {
-    public var id: String { self }
+struct Country {
+    var id = UUID()
+    var name: String
 }
 
 struct MainView: View {
-    let names = [
-        "Hello",
-        "World",
-        "Hello",
-        "World",
-        "Hello",
-        "World",
-        "Hello",
-        "World",
-        "Hello",
-        "World",
+    let countries = [
+        Country(name:"Egypt"),
+        Country(name:"Guinea-Bissau"),
+        Country(name:"Guinée"),
+        Country(name:"Guinée Equatoriale"),
+        Country(name:"India"),
+        Country(name:"Israel"),
+        Country(name:"Jordan"),
+        Country(name:"Kenya"),
+        Country(name:"Kuwait"),
+        Country(name:"Madagascar"),
+        Country(name:"Mali"),
+        Country(name:"Maroc"),
+        Country(name:"Mauritius"),
+        Country(name:"Mozambique"),
+        Country(name:"Niger"),
+        Country(name:"Nigeria"),
+        Country(name:"Oman"),
+        Country(name:"Qatar"),
+        Country(name:"Saudi Arabia"),
+        Country(name:"Sénégal"),
+        Country(name:"South Africa"),
+        Country(name:"Tunisie"),
+        Country(name:"Uganda"),
     ]
     var body: some View {
         List {
