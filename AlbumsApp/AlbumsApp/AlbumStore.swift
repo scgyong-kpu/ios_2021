ce234e9872a943dac4c63a4cb68cde89b41917b3
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-07 16:13:01 +0900

    Album decoder

--- AlbumsApp/AlbumsApp/AlbumStore.swift
+++ AlbumsApp/AlbumsApp/AlbumStore.swift
@@ -6,10 +6,33 @@
 //
 
 import Foundation
-
+struct Album: Codable {
+    var albumTitle: String
+    var artistName: String
+    var image: String
+}
+struct AlbumData: Codable {
+    var albums: [Album]
+}
 class AlbumStore {
     static let singleton = AlbumStore()
+    var albums = [Album]()
     func load() {
-        print("Loading...")
+        guard let url = Bundle.main.url(forResource: "res/albums.json", withExtension: "") else {
+            print("albums.json not found !!")
+            return
+        }
+        print("Loading from: \(url)...")
+        guard let data = try? Data(contentsOf: url) else {
+            print("Could not read from \(url)")
+            return
+        }
+        let decoder = JSONDecoder()
+        guard let albumData = try? decoder.decode(AlbumData.self, from: data) else {
+            print("Decode failed")
+            return
+        }
+        self.albums = albumData.albums
+        print("Loaded Albums: \(albums.count)")
     }
 }
