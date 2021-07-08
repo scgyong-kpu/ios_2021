76bce008cdc42d1a6ab4210346be34e45743ee7a
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-08 17:07:10 +0900

    Long Press and then Drag Gesture

--- GestureApp/GestureApp/ContentView.swift
+++ GestureApp/GestureApp/ContentView.swift
@@ -9,31 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     @State var magnification = CGFloat(1.0)
+    @GestureState var offset = CGSize.zero
+    @State var dragEnabled = false
+    
     var body: some View {
-        let tapGesture = TapGesture(count: 2)
+//        let tapGesture = TapGesture(count: 2)
+//            .onEnded { _ in
+//                NSLog("tapped")
+//            }
+//        let longPressGesture = LongPressGesture(minimumDuration: 3, maximumDistance: 5)
+//            .onEnded { _ in
+//                NSLog("Long pressed")
+//            }
+//        let magGesture = MagnificationGesture(minimumScaleDelta: 0)
+//            .onChanged { value in
+//                print("Mag value = \(value)")
+//                self.magnification = value
+//            }
+//            .onEnded { value in
+//                NSLog("MagGesture Ended: \(value)")
+//            }
+//        let dragGesture = DragGesture()
+//            .updating($offset) { dv, state, transaction in
+//                state = dv.translation
+//            }
+        let lpAndDrag = LongPressGesture(minimumDuration: 1.0)
             .onEnded { _ in
-                NSLog("tapped")
+                self.dragEnabled = true
+                print("drag enabled")
             }
-        let longPressGesture = LongPressGesture(minimumDuration: 3, maximumDistance: 5)
+            .sequenced(before: DragGesture())
+            .updating($offset) { value, state, transaction in
+                switch value {
+                case .first(true):
+                    NSLog("Long press in progress")
+                case .second(true, let drag):
+                    state = drag?.translation ?? .zero
+                default: break
+                }
+            }
             .onEnded { _ in
-                NSLog("Long pressed")
-            }
-        let magGesture = MagnificationGesture(minimumScaleDelta: 0)
-            .onChanged { value in
-                print("Mag value = \(value)")
-                self.magnification = value
-            }
-            .onEnded { value in
-                NSLog("MagGesture Ended: \(value)")
+                self.dragEnabled = false
             }
         return VStack {
             Image(systemName: "hand.point.right.fill")
+                .foregroundColor(dragEnabled ? .green : .blue)
                 .font(.largeTitle)
                 .padding()
                 .border(Color.black, width: 2)
                 .scaleEffect(magnification)
-                .gesture(tapGesture)
-                .gesture(magGesture)
+                .offset(offset)
+                //.gesture(tapGesture)
+                //.gesture(magGesture)
+                .gesture(lpAndDrag)
         }
     }
 }
