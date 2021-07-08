eac3c10b4b19e6e21ff8c609bc0c8bd52d777926
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-08 12:17:38 +0900

    7/8 수업시간에 진행한 내용

--- AlbumsApp/AlbumsApp/ContentView.swift
+++ AlbumsApp/AlbumsApp/ContentView.swift
@@ -11,6 +11,18 @@ extension Album: Identifiable {
     var id: String { albumTitle }
 }
 
+extension Color {
+    static func argb(_ argb: UInt32) -> Color {
+        // argb == 0x12345678
+        // a = 0x12, r = 0x34, g = 0x56, b = 0x78
+        let a = Double(argb >> 24 & 0xFF) / 255.0
+        let r = Double(argb >> 16 & 0xFF) / 255.0
+        let g = Double(argb >> 8 & 0xFF) / 255.0
+        let b = Double(argb & 0xFF) / 255.0
+        return Color(.sRGB, red: r, green: g, blue: b, opacity: a)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         List {
@@ -31,17 +43,45 @@ struct ContentView_Previews: PreviewProvider {
 
 struct AlbumCell: View {
     let album: Album
+    @State var image: Image?
     var body: some View {
         HStack {
-            ImageStore.load(strUrl: album.image)
-            VStack {
+            loadAlbumImage()
+                .resizable()
+                .frame(width: 56, height: 56, alignment: .center)
+            VStack(alignment: .leading) {
                 Text(album.albumTitle)
-                    .font(.subheadline)
-                    .foregroundColor(Color.blue)
-                    .multilineTextAlignment(.leading)
+                    .font(.headline)
+                    .foregroundColor(.argb(0xFF283747))
+                    .frame(height: 50)
                 Text(album.artistName)
-                    .multilineTextAlignment(.leading)
+                    .font(.caption)
+                    .foregroundColor(.argb(0xFF333333))
+                    .lineLimit(1)
             }
         }
     }
+    func loadAlbumImage() -> Image {
+        if let image = image {
+            return image
+        }
+//        if image != nil {
+//            return image!
+//        }
+        return ImageStore.load(strUrl: album.image) { image in
+            self.image = image
+        }
+    }
 }
+
+struct AlbumCell_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            AlbumCell(album: Album(albumTitle: "Hello title", artistName: "Hello name", image: "url"))
+            AlbumCell(album: Album(albumTitle: "Some very long album title blah blah blah hello world", artistName: "World name", image: "url"))
+            AlbumCell(album: Album(albumTitle: "World title", artistName: "A very long artist name World name", image: "url"))
+            AlbumCell(album: Album(albumTitle: "Some very long album title blah blah blah hello world", artistName: "A very long artist name World names jfksd fklsdj fsjd klfjdsklfj dslkjf klsdj fkldsj fklsj dklfj sklfj kldsjf klsdj kl", image: "url"))
+        }
+    }
+}
+
--- AlbumsApp/AlbumsApp/ImageStore.swift
+++ AlbumsApp/AlbumsApp/ImageStore.swift
@@ -10,23 +10,36 @@ import UIKit
 import SwiftUI
 
 class ImageStore {
-    static func load(strUrl: String) -> Image {
+    static var cache = [String: Image]()
+    
+    static func load(strUrl: String, onDownload: @escaping (Image)->Void) -> Image {
+        if let image = cache[strUrl] {
+            NSLog("Using downloaded image for: \(strUrl)")
+            return image
+        }
         guard let url = URL(string: strUrl) else {
             print("Invalid URL:\(strUrl)")
-            return Image(systemName: "wifi")
+            return Image(systemName: "tv.music.note")
         }
         
-        guard let data = try? Data(contentsOf: url) else {
-            print("Failed to load from \(strUrl)")
-            return Image(systemName: "wifi.slash")
-        }
-        
-        guard let uiImage = UIImage(data: data) else {
-            print("Not an image data: \(strUrl)")
-            return Image(systemName: "wifi")
-        }
-        
-        print("Image loaded: \(strUrl)")
-        return Image(uiImage: uiImage)
+        NSLog("start: \(strUrl)")
+        URLSession.shared.dataTask(with: url) { data, resp, err in
+            var image: Image?
+            if let data = data {
+                if let uiImage = UIImage(data: data) {
+                    image = Image(uiImage: uiImage)
+                }
+            }
+
+            OperationQueue.main.addOperation {
+                if image != nil {
+                    cache[strUrl] = image
+                }
+                NSLog("end: \(strUrl)")
+                onDownload(image ?? Image(systemName: "tv.music.note"))
+            }
+        }.resume()
+
+        return Image(systemName: "tv.music.note")
     }
 }
