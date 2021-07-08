b53c8a759a3eafdfcbc662df856b1d4c5df0ed65
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-08 16:35:02 +0900

    Tap  / LongPress Gesture

--- GestureApp/GestureApp/ContentView.swift
+++ GestureApp/GestureApp/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image(systemName: "hand.point.right.fill")
+                .font(.largeTitle)
+                .padding()
+                .border(Color.black, width: 2)
+                .gesture(TapGesture(count: 2)
+                            .onEnded { _ in
+                                NSLog("tapped")
+                            }
+                )
+                .gesture(LongPressGesture(minimumDuration: 3, maximumDistance: 5)
+                            .onEnded { _ in
+                                NSLog("Long pressed")
+                            }
+                )
+        }
     }
 }
 
