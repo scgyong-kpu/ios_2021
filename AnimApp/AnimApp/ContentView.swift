0b1c7716714a4c62abb07d0fa172139c2794fa44
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-08 15:21:39 +0900

    rotation and scale

--- AnimApp/AnimApp/ContentView.swift
+++ AnimApp/AnimApp/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var rotation = 0.0
+    @State var scale: CGFloat = 1.0
     var body: some View {
         Button(action: onBtn) {
             Text("Hit me!")
@@ -17,13 +18,18 @@ struct ContentView: View {
                 .padding()
                 .border(Color.black, width: 2)
                 .rotationEffect(.degrees(rotation))
-                .animation(.linear(duration: 0.3))
+                //.animation(.linear(duration: 0.3))
+                .animation(.spring(response: 1, dampingFraction: 0.2, blendDuration: 0))
+                .scaleEffect(scale)
         }
     }
     func onBtn() {
         self.rotation = self.rotation < 360 ? self.rotation + 60 : 0
+        self.scale = self.scale < 2.8  ? self.scale +  0.3 : 1
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
