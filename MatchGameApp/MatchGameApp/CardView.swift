236ecde03dd43cdf399a1721851de8e10a4ae4a4
Kiyong Kim <scgyong@kpu.ac.kr>
2021-07-09 10:56:44 +0900

    Added card

---
+++ MatchGameApp/MatchGameApp/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  MatchGameApp
+//
+//  Created by Kiyong Kim on 2021/07/09.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
